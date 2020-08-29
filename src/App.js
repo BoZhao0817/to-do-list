23db5560a1824cdbfd0543b788aea762f0815395
Bo Zhao <boz5@illinois.edu>
2020-08-29 02:08:17 -0500

    finish add/delete/complete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+const removeIcon = <i className="far fa-trash-alt"/>
+const completedIcon = <i className="far fa-check-circle"/>
+const incompleteIcon = <i className="far fa-circle"/>
+
 const INIT_TODO = [
-    'create my first react app',
-    'just hard coded staff',
-    'drink water'
+    {
+        text: 'create my first react app',
+        isComplete: false,
+    },
+    {
+        text: 'just hard coded staff',
+        isComplete: false,
+    },
+    {
+        text: 'drink water',
+        isComplete: false,
+    }
 ]
 
 // component 1
-function Todo({todo}) {
+// plug in todos and fucntions
+function Todo({todo, removeTodo, completeTodo, index}) {
     return (
-        <div className="todo">
-            {todo}
+        <div className="todo"
+             onClick={() => completeTodo(index)}>
+
+            <div className="task"
+                 style = {{textDecoration: todo.isComplete ? 'line-through' :''}}
+            >
+                {/*true : false*/}
+                { todo.isComplete ? completedIcon : incompleteIcon}
+                {todo.text}
+            </div>
+            {/*add onclick event on the icon, and the event return a function to delete the item*/}
+            <a className="remove" href="#!" onClick={(e) => removeTodo(e, index)}>
+                { removeIcon }
+            </a>
         </div>
 
     )
@@ -22,7 +48,11 @@ function TodoForm ({ addTodo }) {
     // prevent refresh when submit form
     const handleSubmit = (e) => {
         e.preventDefault();
+        // check if no content added
+        if (!value) return;
         addTodo(value);
+        // clear input form
+        setValue("");
 
     }
     // value is variable that we wanna change, setValue is a function to change this value, defaut value = ''
@@ -41,21 +71,58 @@ function TodoForm ({ addTodo }) {
 
 
 function App() {
-    // hooks to get v dynamically
-    const [todos, setTodo] = useState(INIT_TODO)
+    // hooks to get v dynamically, set the original state
+    // check if localstorage is empty, if so init_todo, else, localStorage
+    // localstorage.clear() clear all
+    // localStorage.removeItems(todos)
+    // local storage可存储多张照片
+    const [todos, setTodo] = useState( JSON.parse(localStorage.getItem('todos'))||INIT_TODO)
+
+    //check state change = componentDidMount
+    // callback是一个空array？check change of the array?
+    useEffect(() => {
+        // store todos in local Stotage
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos])
+
     // functions to add new todos to original list
     const addTodo = (text) =>{
         // add todos before INIT_TODO
-        const newTodo = [text, ...INIT_TODO]
-        setTodo(newTodo)
+        const newTodo = [{ text }, ...INIT_TODO];
+        setTodo(newTodo);
     }
+    //function to remove todo
+    const removeTodo = (e, index) => {
+        // must plug in index to delete certain todo
+        // console.log(index)
+        e.preventDefault();
+        e.stopPropagation();
+        const newTodo = [...todos];
+        newTodo.splice(index, 1);
+        setTodo(newTodo);
+
+    }
+    //function to complete todo
+    const completeTodo = (index) => {
+        // why copy before complete or delete?
+        const newTodos = [...todos];
+        newTodos[index].isComplete = true;
+        setTodo(newTodos);
+    }
+
     return (
      <div className="app">
        <div className="todo-list">
          <TodoForm addTodo = {addTodo}/>
            {
-               todos.map((v) =>(
-                   <Todo key={v} todo={v}/>
+               todos.map((v, index) =>(
+                   <Todo
+                       removeTodo ={removeTodo}
+                       completeTodo={completeTodo}
+                       index={index}
+                       //*key={index}就可以，只要是个唯一的值
+                       // key props无法读取 不是给child的， need to be unique
+                       key={index} todo={v}/>
                ))
            }
        </div>
